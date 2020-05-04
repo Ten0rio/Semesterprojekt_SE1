5508937b1b72a44616a23bab468d823194136e67
Fabian <fabian.bender@smail.inf.h-brs.de>
2020-05-04 17:16:02 +0200

    Automat, IF und Test

---
+++ src/Interfaces/AutomatIF.java
@@ -0,0 +1,8 @@
+package Interfaces;
+
+import Klassen.Parkschein;
+
+public interface AutomatIF {
+    Parkschein parkscheinbezahlen(Parkschein other);
+
+}
--- src/Klassen/Automat.java
+++ src/Klassen/Automat.java
@@ -1,4 +1,22 @@
 package Klassen;
 
-public class Automat {
+import Interfaces.AutomatIF;
+
+public class Automat implements AutomatIF {
+
+    double gewinn;
+
+    public Automat(){
+        this.gewinn = 0;
+    };
+
+    @Override
+    public Parkschein parkscheinbezahlen(Parkschein zubezahlen) {
+        //Theoretisches Hochrechnen ohne Zeitdifferenz
+        this.gewinn =+ zubezahlen.Timestamp * 3;
+        //Ist bezahlt set true?
+        zubezahlen.istBezahlt();
+        //returned bezahltes Ticket
+        return zubezahlen;
+    }
 }
