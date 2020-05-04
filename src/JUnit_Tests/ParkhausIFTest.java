9a9f4e5767ede1c6bbd42abb43d05813ab9e4277
Ten0rio <63906477+Ten0rio@users.noreply.github.com>
2020-05-04 17:14:40 +0200

    So Klassen pseudocode

---
+++ src/JUnit_Tests/ParkhausIFTest.java
@@ -0,0 +1,22 @@
+package JUnit_Tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParkhausIFTest {
+
+    @org.junit.jupiter.api.Test
+    void einfahren() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void ausfahren() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getParkPlaetzeMax() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getParkplatzbelegt() {
+    }
+}
--- src/Klassen/Parkhaus.java
+++ src/Klassen/Parkhaus.java
@@ -1,4 +1,30 @@
 package Klassen;
 
-public class Parkhaus {
+import Interfaces.ParkhausIF;
+
+public class Parkhaus implements ParkhausIF {
+
+    int parkPlaetzeMax;
+    int parkplatzbelegt;
+    boolean schrankeOeffnet;
+
+    @Override
+    public void einfahren() {
+
+    }
+
+    @Override
+    public void ausfahren() {
+
+    }
+
+    @Override
+    public int getParkPlaetzeMax() {
+        return 0;
+    }
+
+    @Override
+    public int getParkplatzbelegt() {
+        return 0;
+    }
 }
