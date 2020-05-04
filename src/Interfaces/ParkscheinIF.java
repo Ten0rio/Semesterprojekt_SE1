08663162e5bf2fa6b21201f50a70098b90c41316
lwerte2s <luwer3@googlemail.com>
2020-05-04 17:07:57 +0200

    Parschein

--- src/Interfaces/ParkscheinIF.java
+++ src/Interfaces/ParkscheinIF.java
@@ -2,4 +2,5 @@ package Interfaces;
 
 public interface ParkscheinIF {
 
+	public boolean istBezahlt();
 }
--- src/Klassen/Parkschein.java
+++ src/Klassen/Parkschein.java
@@ -1,4 +1,17 @@
 package Klassen;
 
-public class Parkschein {
+import Interfaces.ParkscheinIF;
+
+public class Parkschein implements ParkscheinIF {
+	
+	boolean bezahlt;
+	long Timestamp = System.currentTimeMillis();
+
+
+	@Override
+	public boolean istBezahlt() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
 }
