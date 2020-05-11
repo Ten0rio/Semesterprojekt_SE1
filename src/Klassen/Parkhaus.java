b997b609caa5b20f3e63d9fe61ba1065166cbc9e
Fabian <fabian.bender@smail.inf.h-brs.de>
2020-05-11 16:07:06 +0200

    New Servlet Parkhaus

--- src/Klassen/Parkhaus.java
+++ src/Klassen/Parkhaus.java
@@ -2,16 +2,39 @@ package Klassen;
 
 import Interfaces.ParkhausIF;
 
-public class Parkhaus implements ParkhausIF {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+public class Parkhaus extends HttpServlet implements ParkhausIF{
+
+    private String message;
     private int parkPlaetzeMax;
     private int parkplatzbelegt;
     private boolean schrankeOeffnet;
+
+    public Parkhaus(){}
 
     public Parkhaus(int parkPlaetzeMax){
         this.parkPlaetzeMax=parkPlaetzeMax;
     }
 
+    @Override
+    public void init() throws ServletException {
+        // Do required initialization
+        this.message = "Hello World";
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+
+        // Actual logic goes here.
+        PrintWriter out = response.getWriter();
+        out.println("<h1>" + message + "</h1>");
+    }
     @Override
     public void einfahren() {
     }
--- src/Servlets/DemoServlet.java
+++ src/Servlets/DemoServlet.java
@@ -132,7 +132,7 @@ public class DemoServlet extends HttpServlet {
         return avg;
     }
 
-    private int getPersistentTotalCars() {
+    private Integer getPersistentTotalCars() {
         Integer total;
         ServletContext application = getApplication();
         total = (Integer) application.getAttribute("total");
