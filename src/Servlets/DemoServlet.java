17ba4476b557318df7637935cb123f9f809ae486
Ten0rio <63906477+Ten0rio@users.noreply.github.com>
2020-05-11 11:01:06 +0200

    mit parkhaus

--- src/Servlets/DemoServlet.java
+++ src/Servlets/DemoServlet.java
@@ -7,9 +7,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "Servlet")
-public class Servlet extends HttpServlet {
-
+@WebServlet(name = "DemoServlet")
+public class DemoServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
