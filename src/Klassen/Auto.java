03436e3a523426b57ce06872f58e91b742328e3c
lwerte2s <luwer3@googlemail.com>
2020-05-27 12:13:51 +0200

    aufgabe 5.3 (prototyp2

---
+++ src/Klassen/Auto.java
@@ -0,0 +1,41 @@
+package Klassen;
+
+import Interfaces.AutoIF;
+
+public class Auto implements AutoIF {
+
+    String number = "";
+    String parkplatz = "";
+    String parkzeit = "";
+
+    public Auto(String number, String parkplatz, String parkzeit){
+        this.number = number;
+        this.parkplatz = parkplatz;
+        this.parkzeit = parkzeit;
+    }
+
+
+    public String getNumber(){
+        return number;
+    }
+    public String getParkzeit() {
+        return parkzeit;
+    }
+    public String getParkplatz() {
+        return parkplatz;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public void setParkplatz(String parkplatz) {
+        this.parkplatz = parkplatz;
+    }
+
+    public void setParkzeit(String parkzeit) {
+        this.parkzeit = parkzeit;
+    }
+
+
+}
