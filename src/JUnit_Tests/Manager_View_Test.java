0e3c1e773917020931a2aae12c974395b07f3acd
Ten0rio <63906477+Ten0rio@users.noreply.github.com>
2020-10-07 21:13:00 +0200

    Tests von perioden Dauer

--- src/JUnit_Tests/Manager_View_Test.java
+++ src/JUnit_Tests/Manager_View_Test.java
@@ -1,8 +1,6 @@
 package JUnit_Tests;
 
-import Klassen.Manager_View;
-import Klassen.Parkhaus_Fachlogik;
-import Klassen.WochenEinnahmen_View;
+import Klassen.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,23 +13,30 @@ class Manager_View_Test {
 
 
     Parkhaus_Fachlogik parkhaus_fachlogik;
-    Manager_View manager_view;
+    Manager_View manager_view_Woche;
+    Manager_View manager_view_Tag;
+    Manager_View manager_view_Monat;
 
     @BeforeEach
     void setup(){
         parkhaus_fachlogik = new Parkhaus_Fachlogik();
-        manager_view = new WochenEinnahmen_View(parkhaus_fachlogik);
-        parkhaus_fachlogik.add(manager_view);
+        manager_view_Woche = new WochenEinnahmen_View(parkhaus_fachlogik);
+        manager_view_Tag = new TagesEinnahmen_View(parkhaus_fachlogik);
+        manager_view_Monat = new MonatsEinnahmen_View(parkhaus_fachlogik);
+
+        parkhaus_fachlogik.add(manager_view_Woche);
+        parkhaus_fachlogik.add(manager_view_Tag);
+        parkhaus_fachlogik.add(manager_view_Monat);
     }
 
 
     @Test
     @DisplayName("update() bei leerem Parkhaus sollte einnahmen auf 0 setzen")
     void updateleer() {
-        manager_view.update();
-        assertEquals(0,manager_view.getEinnahmen());
+        manager_view_Woche.update();
+        assertEquals(0, manager_view_Woche.getEinnahmen());
         parkhaus_fachlogik.removeParkschein();
-        assertEquals(0,manager_view.getEinnahmen());
+        assertEquals(0, manager_view_Woche.getEinnahmen());
     }
 
 
@@ -39,7 +44,7 @@ class Manager_View_Test {
     @DisplayName("Werte höher 0")
     void update(){
         parkhaus_fachlogik.addParkschein(new String[]{"1","2","3","4","500"});
-        assertEquals(5,manager_view.getEinnahmen());
+        assertEquals(5, manager_view_Woche.getEinnahmen());
     }
     @Test
     @DisplayName("Wert gleich 5")
@@ -47,15 +52,32 @@ class Manager_View_Test {
         parkhaus_fachlogik.addParkschein(new String[]{"1","2","3","4","500"});
         parkhaus_fachlogik.addParkschein(new String[]{"1","2","3","4","200"});
         parkhaus_fachlogik.removeParkschein();
-        assertEquals(5,manager_view.getEinnahmen());
+        assertEquals(5, manager_view_Woche.getEinnahmen());
     }
+    @Test
+    @DisplayName("der in date gespeicherte Tag ist länger als ein Tag her und die Einnahmen müssen zurück gesetzt werden")
+    void updatedatetag(){
+
+        parkhaus_fachlogik.addParkschein(new String[]{"leave","1153","0","1500","150","2b0f8a8452b04d02b98d5193216a8cab","#a6eedf","8","family","Trike","1153" });
+        parkhaus_fachlogik.addParkschein(new String[]{"leave","1154","87000000","1500","150","2b0f8a8452b04d02b98d5193216a8cab","#a6eedf","8","family","Trike","1154" });
+        assertEquals(1.5, manager_view_Tag.getEinnahmen());
+    }
+
 
     @Test
     @DisplayName("der in date gespeicherte Tag ist länger als eine Woche her und die Einnahmen müssen zurück gesetzt werden")
-    void updatedate(){
+    void updatedateWoche(){
         parkhaus_fachlogik.addParkschein(new String[]{"leave","1153","0","1500","150","2b0f8a8452b04d02b98d5193216a8cab","#a6eedf","8","family","Trike","1153" });
         parkhaus_fachlogik.addParkschein(new String[]{"leave","1154","1000000000","1500","150","2b0f8a8452b04d02b98d5193216a8cab","#a6eedf","8","family","Trike","1154" });
-        assertEquals(1.5, manager_view.getEinnahmen());
+        assertEquals(1.5, manager_view_Woche.getEinnahmen());
+    }
+
+    @Test
+    @DisplayName("der in date gespeicherte Tag ist länger als einen Monat her und die Einnahmen müssen zurück gesetzt werden")
+    void updatedateMonat(){
+        parkhaus_fachlogik.addParkschein(new String[]{"leave","1153","0","1500","150","2b0f8a8452b04d02b98d5193216a8cab","#a6eedf","8","family","Trike","1153" });
+        parkhaus_fachlogik.addParkschein(new String[]{"leave","1154","3000000000","1500","150","2b0f8a8452b04d02b98d5193216a8cab","#a6eedf","8","family","Trike","1154" });
+        assertEquals(1.5, manager_view_Monat.getEinnahmen());
     }
 
     @Test
@@ -65,14 +87,14 @@ class Manager_View_Test {
         parkhaus_fachlogik.addParkschein(new String[]{"leave","1153","0","1500","150","2b0f8a8452b04d02b98d5193216a8cab","#a6eedf","8","family","Trike","1153" });
         parkhaus_fachlogik.addParkschein(new String[]{"leave","1153","1000000000","1500","150","2b0f8a8452b04d02b98d5193216a8cab","#a6eedf","8","family","Trike","1153" });
         parkhaus_fachlogik.addParkschein(new String[]{"leave","1154","2000000000","1500","150","2b0f8a8452b04d02b98d5193216a8cab","#a6eedf","8","family","Trike","1154" });
-        assertEquals(1.5, manager_view.getEinnahmen());
+        assertEquals(1.5, manager_view_Woche.getEinnahmen());
         parkhaus_fachlogik.removeParkschein();
-        assertEquals(1.5, manager_view.getEinnahmen());
+        assertEquals(1.5, manager_view_Woche.getEinnahmen());
         parkhaus_fachlogik.removeParkschein();
-        assertEquals(5.5, manager_view.getEinnahmen());
+        assertEquals(5.5, manager_view_Woche.getEinnahmen());
         parkhaus_fachlogik.removeParkschein();
-        assertEquals(4, manager_view.getEinnahmen());
+        assertEquals(4, manager_view_Woche.getEinnahmen());
         parkhaus_fachlogik.removeParkschein();
-        assertEquals(0 , manager_view.getEinnahmen());
+        assertEquals(0 , manager_view_Woche.getEinnahmen());
     }
 }
